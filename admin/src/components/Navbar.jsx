0c77f9d1ab7b3d3fc568183153cf37aa3c8c63c1
Priyanshi Rathore <priyanshirathore624@gmail.com>
2025-04-13 12:48:33 +0530

    Add Doctor Functionality in admin panel

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react';
 import { assets } from '../assets/assets';
 import { AdminContext } from '../context/AdminContext';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
   const { aToken ,setAToken} = useContext(AdminContext);
 
-  const navigate= useNavigate()
+  const navigate= useNavigate
 
   const logout = () => {
     navigate('/')
