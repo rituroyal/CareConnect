0230878ea98d6f67786289e18ca22bbe54c81856
yamistarry <kumarivimlesh016@gmail.com>
2025-04-13 18:48:29 +0530

    display doctor list

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import adminRouter from './routes/adminRoute.js'
+import doctorRouter from './routes/doctorRoute.js'
 
 //app config
 const app = express()
@@ -16,7 +17,7 @@ app.use(cors())
 
 // api endpoints
 app.use('/api/admin', adminRouter)
-// localhost:4000/api/admin/add-doctor
+app.use('api/doctor',doctorRouter)
 
 app.get('/', (req, res) => {
     res.send('API WORKING Great')
