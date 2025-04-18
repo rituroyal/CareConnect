ea225992c89708380ca3d87dbc03f1cced023b25
yamistarry <kumarivimlesh016@gmail.com>
2025-04-18 11:03:30 +0530

    Appointment pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import About from './Pages/About'
 import Contact from './Pages/Contact'
 import MyProfile from './Pages/MyProfile'
 import Navbar from './components/Navbar';
+import MyAppointments from './Pages/MyAppointments'
+import Appointment from './Pages/Appointment';
 import Footer from './components/Footer';
 
 
@@ -22,6 +24,8 @@ const App = () => {
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/my-profile' element={<MyProfile/>}/>
+        <Route path='/my-appointments' element={<MyAppointments/>}/>
+        <Route path='/appointment/:docId' element={<Appointment/>}/>
       </Routes>
       <Footer />
     </div>
